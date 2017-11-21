a864de0cf228819601fd336a5a5035dec75a6082
Anicet Serge <tghil7@aol.com>
2017-11-21 16:35:11 -0600

    Removed carousel on home page and replaced with fixed image

--- view/homePage.php
+++ view/homePage.php
@@ -34,10 +34,10 @@
                                         </ul>
                                   </div>
                         </div>
-				  
-				  <div id="searchSection"  >
-                                      <div class="carousel-caption">
-				    <p  id="headerWord">Learn about athletic training</p>
+                        <div id="infoDiv">	  
+				  <div id="searchSection" >
+                                      <div>
+				    <p id="headerWord">Learn about athletic training</p>
                                     <p id="headDetails">Use the diagnostic page to provide a possible diagnostic to your symptoms.<br>
                                         You can also try to search for an athletic term you may not be familiar with <br/>
                                         using the search box below. </p><br/>
@@ -49,30 +49,10 @@
                                     </form>                   
 				                  
 			          </div>
+                        </div>
+                               
       
-		          <div id ="myCarousel" class="carousel slide" data-ride ="carousel" >
-				  
-				  
-				        <ol class="carousel-indicators">
-						    <li data-target="#myCarousel" data-slide-to="0" class= "active"></li>
-							<li data-target="#myCarousel" data-slide-to="1"></li>
-							<li data-target="#myCarousel" data-slide-to="2"></li>
-		                    			  
-				        </ol> 
-
-                              <div class="carousel-inner" role="listbox">
-							     <div class="item active">
-                                                                 <img src = "../images/hoop.jpg" alt="hoop" class="mainImage"/>
-								 </div>
-								 <div class="item">
-                                                                     <img src = "../images/athletegirl.jpg" alt="runner" class="mainImage" />
-								 </div>
-								 <div class="item">
-                                                                     <img src = "../images/athletic.JPG" alt="soccer" class="mainImage" />
-								 </div>
-							  </div>
-                                                           					  
-			      </div>		
+		         
 		 
      <footer class="footer"><small><i>Copyright &copy; <?php echo date('Y')  ?> All rights reserved. The Athletic Trainer.<a href="mailto:webmaster@athletictrainer.com">
                             webmaster@athletictrainer.com</i></small></footer>		 
